3b2bfd6465e5b297f434114b9896e03185ee1d76
Twalaght <jonathanchapman6799@gmail.com>
2020-01-02 00:00:37 +1030

    Updated main script and readme

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ cd TSAR
 bash TSAR.sh
 ```
 
-## TSAR is a work in progress
+## What TSAR does:
+I'm a lazy person. I don't like having to do things more than once. TSAR is my portable kit to configure a fresh Arch install to my liking. These scripts probably are not to useful to anybody who is not me, but it also holds my dotfiles, if you are interested in them.
+
+Shameless derivative or LARBS by Luke Smith
--- TSAR.sh
+++ TSAR.sh
@@ -1,11 +1,101 @@
 #!/bin/sh
 
-welcomemsg() { \
-	dialog --title "Welcome!" --msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
-	}
+##TODO Wrap installs into 1 func
 
 
-welcomemsg || error "User exited."
+# Displays menus to guide the user through installation
+menus() {
+	# Intro message to TSAR
+	clear
+	if ! dialog --title "TSAR" --yesno "Welcome to TSAR - Twa's Scripted Auto Rice\\n\\nThis script will automatically install an up to date, i3-gaps Arch Linux rice.\\n\\nOptionally, program installation or dotfile merging can be omitted" 12 50; then
+		clear
+		exit
+	fi
+
+	# Gives the user the option of which parts to run
+	edition="$(dialog --title "TSAR method" --menu "Select what you wish TSAR to do to your system:" 10 60 2 programs "Only install programs" files "Only copy dotfiles" both "Install programs and copy dotfiles" 3>&1 1>&2 2>&3 3>&1 && exit)" 
+	clear
+}
+
+# Installs a standard package with pacman
+packageinstall() {
+	if sudo pacman --noconfirm --needed -S "$1" >/dev/null 2>&1; then
+		echo "done"
+	else
+		error "$1"
+	fi
+}
+
+# Installs AUR packages with trizen
+aurinstall() {
+	if trizen -S --noconfirm --needed "$1" >/dev/null 2>&1; then
+		echo "done"
+	else
+		error "$1"
+	fi
+}
+
+# Installs python packages with pip
+pipinstall() {
+	if sudo pip3 install "$1" >/dev/null 2>&1; then
+		echo "done"
+	else
+		error "$1"
+	fi
+	
+}
+
+# Logs programs that trigger errors when installing
+error() {
+	echo "ERROR - Written to log file"
+	echo "$1" was unable to be installed >> log
+}
+
+# Copies TSARs dotfiles
+dotfiles() {
+
+}
+
+# Installs TSARs set of programs
+proginstall() {
+	# Counts the total number of programs to be installed
+	total=$(($(wc -l < progs.csv) - 1))
+
+	# Iterates through the csv, discarding the opening line
+	{ read -r
+	while IFS=, read -r tag program comment; do
+		# Number of the current program being installed
+		n=$((n+1))
+		
+		# Remove the carriage return from the comment
+		comment=$(echo "$comment" | sed -e 's/\r//g')
+
+		# Displays the progress on installation
+		#echo -n "[$n/$total] $program : $comment... "
+		printf "%s" "[$n/$total] $program : $comment... "
+
+		# Runs the installation command based on the tag
+		case "$tag" in
+			"A") aurinstall "$program" "$comment" ;;
+			"P") pipinstall "$program" "$comment" ;;
+			*) packageinstall "$program" "$comment" ;;
+		esac
+	done } < progs.csv
+}
 
 
-echo "This doesn't do anything yet"
+menus
+
+if [ -z "$edition" ]; then
+	exit
+elif [ "$edition" = both ]; then
+	dotfiles
+	proginstall
+elif [ "$edition" = files ]; then
+	dotfiles
+elif [ "$edition" = programs ]; then
+	proginstall
+fi
+
+echo
+echo "Installation complete!"
