0d963f0a38dae88a17f628259860e34bb3e044da
Twalaght <jonathanchapman6799@gmail.com>
2020-11-17 00:47:40 +1030

    Removed old tsar dotfiles

--- dotfiles/.profile
+++
@@ -1,2 +0,0 @@
-# Add the user scripts folder to path
-export PATH="${PATH}:$HOME/.scripts/"
--- dotfiles/.scripts/scriptUpdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Clone the TSAR repo containing the scripts
-git -C /tmp clone --quiet https://github.com/Twalaght/TSAR
-# Copy the scripts folder to home
-cp -r /tmp/TSAR/dotfiles/.scripts ~
-# Remove the temp repo
-rm -rf /tmp/TSAR
